b99597bc5f006f723facb7c7ff831eaa9294054a
renlin-code <renelj1997@gmail.com>
2022-05-11 22:59:22 +0500

    Añadidos estilos y funcionalidad a interfaz de favorites

--- script/main.js
+++ script/main.js
@@ -1,80 +1,64 @@
 "use strict"
 
-const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2";
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=6";
 const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites";
 const API_URL_FAVORITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
 const API_URL_UPLOAD = "https://api.thecatapi.com/v1/images/upload";
 
 
 const randomCatsError = document.getElementById("randomCatsError");
-const reloadButton = document.getElementById("reloadButton");
 const uploadButton = document.getElementById("uploadButton");
 
+
+
+//FUNCTIONS FOR FETCH
 async function loadRandomCats() {
     const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
     const saveButton1 = document.getElementById("saveButton1");
     const saveButton2 = document.getElementById("saveButton2");
+    const saveButton3 = document.getElementById("saveButton3");
+    const saveButton4 = document.getElementById("saveButton4");
+    const saveButton5 = document.getElementById("saveButton5");
+    const saveButton6 = document.getElementById("saveButton6");
 
     saveButton1.addEventListener("click", () => {
         saveFavoriteCat(data[0].id);
     });
     saveButton2.addEventListener("click", () => {
         saveFavoriteCat(data[1].id)
+    });
+    saveButton3.addEventListener("click", () => {
+        saveFavoriteCat(data[2].id)
     });    
-    
+    saveButton4.addEventListener("click", () => {
+        saveFavoriteCat(data[3].id)
+    });    
+    saveButton5.addEventListener("click", () => {
+        saveFavoriteCat(data[4].id)
+    });    
+    saveButton6.addEventListener("click", () => {
+        saveFavoriteCat(data[5].id)
+    });    
+
     if (res.status !== 200) {
         randomCatsError.innerHTML = `Somethig is wrong: ${res.status}`
     } else {
         const img1 = document.getElementById("img1");
         const img2 = document.getElementById("img2");
-    
+        const img3 = document.getElementById("img3");
+        const img4 = document.getElementById("img4");
+        const img5 = document.getElementById("img5");
+        const img6 = document.getElementById("img6");
+
         img1.src = data[0].url;
-        img2.src = data[1].url;   
+        img2.src = data[1].url;  
+        img3.src = data[2].url;   
+        img4.src = data[3].url;   
+        img5.src = data[4].url;   
+        img6.src = data[5].url;   
+ 
         console.log(data) 
-    }
-}
-
-async function loadFavoriteCats() {
-    const res = await fetch(API_URL_FAVORITES, {
-        method: "GET",
-        headers: {
-            "X-API-KEY": "a3b9a83e-f120-4764-bb3b-295fdae71ead"
-        }
-    });
-    const data = await res.json();
-
-    if (res.status !== 200) {
-        randomCatsError.innerHTML = `Somethig is wrong: ${res.status} ${data.message}`
-    } else {
-        console.log(data);
-        const section = document.getElementById("favoriteCatsSection");
-        section.innerHTML = "";
-        const h1 = document.createElement("h1");
-        const h1Text = document.createTextNode("Favorites cats");
-        
-        h1.appendChild(h1Text);
-        section.appendChild(h1);
-
-        data.forEach(cat => {
-            const article = document.createElement("article");
-            const img = document.createElement("img");
-            const btn = document.createElement("button");
-            const btnText = document.createTextNode("Delete");
-
-            btn.addEventListener("click",() => {
-                deleteFavoriteCat(cat.id)
-            })
-
-            img.src = cat.image.url;
-            img.height = "300px";
-            img.width = "300px";
-
-            btn.appendChild(btnText);
-            article.appendChild(img);
-            article.appendChild(btn);
-            section.appendChild(article);
-            })
     }
 }
 
@@ -96,6 +80,37 @@ async function saveFavoriteCat(id) {
     } else {
         console.log("Cat saved");
         loadFavoriteCats();
+    }
+}
+
+async function loadFavoriteCats() {
+    const res = await fetch(API_URL_FAVORITES, {
+        method: "GET",
+        headers: {
+            "X-API-KEY": "a3b9a83e-f120-4764-bb3b-295fdae71ead"
+        }
+    });
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        randomCatsError.innerHTML = `Somethig is wrong: ${res.status} ${data.message}`
+    } else {
+        console.log(data);
+        const grid = document.getElementById("grid");
+
+        grid.innerHTML = "";
+        
+        data.forEach(cat => {
+            const gridElement = document.createElement("div");
+
+
+            // btn.addEventListener("click",() => {
+            //     deleteFavoriteCat(cat.id)
+            // })
+
+            gridElement.style.backgroundImage = `url(${cat.image.url}`;
+            grid.appendChild(gridElement);
+            })
     }
 }
 
@@ -140,6 +155,27 @@ async function uploadCat() {
 
 loadRandomCats();
 loadFavoriteCats();
-reloadButton.addEventListener("click", loadRandomCats);
-uploadButton.addEventListener("click", uploadCat);
+// uploadButton.addEventListener("click", uploadCat);
+
+
+//NAVIGATION FUNCTIONS
+const openFavorites = () => {
+    const favoriteCatsSection = document.getElementById("favoriteCatsSection");
+    const header = document.querySelector("header");
+    const randomCatsSection = document.getElementById("randomCatsSection");
+
+    favoriteCatsSection.style.transform = "translateX(-100%)";
+    header.style.transform = "translateX(-100%)";
+    randomCatsSection.style.transform = "translateX(-100%)";
+};
+
+const closeFavorites = () => {
+    const favoriteCatsSection = document.getElementById("favoriteCatsSection");
+    const header = document.querySelector("header");
+    const randomCatsSection = document.getElementById("randomCatsSection");
+
+    favoriteCatsSection.style.transform = "translateX(0)";
+    header.style.transform = "translateX(0)";
+    randomCatsSection.style.transform = "translateX(0)";
+};
 
